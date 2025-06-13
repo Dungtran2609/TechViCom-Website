c0a02eaf8ba8e9d6ce2e38e32f1ba42aa56044d5
nhung <nhungphph49974@gmail.com>
2025-06-13 22:38:57 +0700

    Thêm tính năng mới cho nhánh nhung

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\Product\CategoryController;
+use App\Http\Controllers\Admin\Product\BrandController;
+
 
 
 // Chuyển hướng trang chủ vào dashboard
@@ -18,5 +20,15 @@ Route::prefix('admin')->name('admin.')->group(function () {
     Route::get('products/categories/trashed', [CategoryController::class, 'trashed'])->name('products.categories.trashed');
     Route::post('products/categories/{id}/restore', [CategoryController::class, 'restore'])->name('products.categories.restore');
     Route::delete('products/categories/{id}/force-delete', [CategoryController::class, 'forceDelete'])->name('products.categories.force-delete');
-    Route::resource('products/categories', CategoryController::class)->names('products.categories');;
+    Route::resource('products/categories', CategoryController::class)->names('products.categories');
+    ;
+    // Brands routes
+    
+
+    Route::get('brands/trashed', [BrandController::class, 'trashed'])->name('brands.trashed');
+    Route::post('brands/{id}/restore', [BrandController::class, 'restore'])->name('brands.restore');
+    Route::delete('brands/{id}/force-delete', [BrandController::class, 'forceDelete'])->name('brands.force-delete');
+    Route::resource('brands', BrandController::class)->names('brands');
+
+
 });
